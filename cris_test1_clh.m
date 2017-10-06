43f0575adadcf9ac203c43fae7430e0224abd04c
Christopher Hepplewhite <chepplew@umbc.edu>
2017-10-06 17:39:31 -0500

    mmmm

--- cris_test1_clh.m
+++ cris_test1_clh.m
@@ -9,20 +9,20 @@
 % to get "IASI CrIS".  Compare IASI CrIS to true CrIS.
 %
 
+addpath /home/motteler/cris/ccast/source
+addpath /home/motteler/cris/airs_decon/test
+addpath /home/motteler/cris/airs_decon/source
+addpath /asl/packages/iasi_decon/
+
+warning('off', 'MATLAB:imagesci:hdf:removalWarningHDFSD');
+
 %-----------------
 % test parameters
 %-----------------
 
-addpath /home/motteler/cris/ccast/source
-addpath /home/motteler/cris/airs_decon/test
-addpath /home/motteler/cris/airs_decon/source
-
-warning('off', 'MATLAB:imagesci:hdf:removalWarningHDFSD');
-
-% test params
 band = 'LW';
 hapod = 1;
-app   = 'noapp';
+app   = 'app';
 
 % opts for inst_params and iasi2cris
 opt1 = struct;
@@ -70,10 +70,8 @@ if ~hapod [rad4, frq4] = iasi2cris(rad1, frq1, opt1); end
 if hapod [rad4_ham, frq4] = iasi2cris(rad1, frq1, opt1); end
 
 % option to apodize true CrIS
-if hapod
-  app = 'hamm';
-  rad2_ham = hamm_app(rad2);
-end
+if hapod  app = 'hamm';   rad2_ham = hamm_app(rad2); end
+if ~hapod app = 'noapp';  end
 
 %-----------------
 % stats and plots
@@ -93,7 +91,7 @@ pv2 = max(frq2(i2)) + 10;
 
 % IASI and CrIS spectra
 figure(1); clf; j = 1; 
-plot(frq1, bt1(:,j), frq2, bt2(:,j), frq4, bt4(:,j))
+plot(frq1, bt1(:,j), frq2, bt2(:,j), frq4, bt4_ham(:,j))
 ax(1)=pv1; ax(2)=pv2; ax(3)=200; ax(4)=300; axis(ax)
 legend('true IASI', 'true CrIS', 'IASI CrIS', ...
        'location', 'southeast')
@@ -103,13 +101,13 @@ grid on; zoom on
 % saveas(gcf, sprintf('iasi_cris_spec_%s_%s_%s', band,opt1.resmode,app), 'png')
 
 % IASI CrIS minus true CrIS mean
-figure(4); clf
+figure(2); clf
 h1=subplot(2,1,1);
 [i2, i4] = seq_match(frq2, frq4);
 %plot(frq2(i2), mean(bt4(i4,:) - bt2(i2,:), 2),'LineWidth',1); hold on;
-plot(frq2(i2), 1000*mean(bt4_ham(i4,:) - bt2_ham(i2,:),2),'LineWidth',1.5);
+plot(frq2(i2), mean(bt4_ham(i4,:) - bt2_ham(i2,:),2),'LineWidth',1.5);
 %ax = axis; ax(1)=pv1; ax(2)=pv2; axis(ax); xlabel('wavenumber'); 
-axis([pv1 pv2 -0.045 4.5]);ylabel('dBT (mK)')
+axis([pv1 pv2 -0.045 0.045]);ylabel('dBT (mK)')
 %title(sprintf('IASI CrIS - true CrIS %s mean & stdev %s', band,app));
 grid on; zoom on
 
--- cris_test5_sno.m
+++ cris_test5_sno.m
@@ -16,17 +16,18 @@
 %-----------------
 
 % set paths to asl libs
-addpath ../source
-addpath ../h4tools
+addpath /asl/packages/airs_decon/test        % <- original run directory
+addpath /asl/packages/airs_decon/source
+addpath /asl/packages/airs_decon/h4tools
 addpath /asl/packages/ccast/source
 
 warning('off','MATLAB:imagesci:hdf:removalWarningHDFSD');
 
 % test params
-band = 'MW';            % cris band
+band = 'MW';            % cris band {'LW','MW','SW'}
 hapod = 1;              % flag for Hamming apodization
 dvb = 0.1;              % deconvolution frequency step
-bfile = 'bconv4.mat';   % deconvolution temp file
+bfile = '/asl/s1/chepplew/tmp/bconv4.mat';   % deconvolution temp file
 
 % kcarta test data
 kcdir = '/home/motteler/cris/sergio/JUNK2012/';
@@ -77,28 +78,29 @@ clear d1 vkc rkc
 opt1.dvb     = dvb;
 opt1.bfile   = bfile;
 opt1.hapod   = hapod;
-opt1.resmode = 'hires2';
+opt1.resmode = 'lowres';  % 'hires2';
 
+if ~hapod [rad4, frq4, opt2]    = airs2cris(rad2, frq2, sfile, opt1); end
 if hapod [rad4_ham, frq4, opt2] = airs2cris(rad2, frq2, sfile, opt1); end
 rad3 = opt2.brad;
 bfrq = opt2.bfrq;
 
 % option to apodize true CrIS
-if hapod
-  rad1_ham = hamm_app(rad1);
-end
+if hapod  rad1_ham = hamm_app(rad1);  end
+if ~hapod rad1     = hamm_app(rad1);  end
 
 %-----------------
 % stats and plots
 %-----------------
 
 % take radiances to brightness temps
-bt1 = real(rad2bt(frq1, rad1));   % true CrIS
-bt2 = real(rad2bt(frq2, rad2));   % true AIRS
-bt3 = real(rad2bt(bfrq, rad3));   % deconvolved AIRS
-bt4 = real(rad2bt(frq4, rad4));   % AIRS CrIS
-bt1_ham = real(rad2bt(frq1, rad1_ham));
-bt4_ham = real(rad2bt(frq4, rad4_ham));
+bt1 = real(rad2bt(frq1, rad1));                % true CrIS
+bt2 = real(rad2bt(frq2, rad2));                % true AIRS
+bt3 = real(rad2bt(bfrq, rad3));                % deconvolved AIRS
+if ~hapod bt4 = real(rad2bt(frq4, rad4));  end % AIRS CrIS
+if ~hapod bt1 = real(rad2bt(frq1, rad1));  end
+if hapod bt1_ham = real(rad2bt(frq1, rad1_ham));  end
+if hapod bt4_ham = real(rad2bt(frq4, rad4_ham));  end
 
 % plot parameters
 [i1, i4] = seq_match(frq1, frq4); 
@@ -111,7 +113,7 @@ else psf = 2.0; app = 'noap'; end
 % AIRS and CrIS spectra
 figure(1); clf; j = 1; 
 set(gcf, 'Units','centimeters', 'Position', [4, 10, 24, 16])
-plot(frq1, bt1(:,j), frq2, bt2(:,j), bfrq, bt3(:,j), frq4, bt4(:,j))
+plot(frq1, bt1(:,j), frq2, bt2(:,j), bfrq, bt3(:,j), frq4, bt4_ham(:,j))
 ax(1)=pv1; ax(2)=pv2; ax(3)=180; ax(4)=320; axis(ax)
 legend('true CrIS', 'true AIRS', 'AIRS dec', 'AIRS CrIS', ...
        'location', 'southeast')
@@ -123,26 +125,26 @@ pname = sprintf('airs_cris_spec_%s_%s', band, app);
 % export_fig([pname, '.pdf'], '-m2', '-transparent')
 
 % AIRS CrIS minus true CrIS mean
-figure(4); clf
+figure(2); clf
 set(gcf, 'Units','centimeters', 'Position', [4, 10, 24, 16])
-h1=subplot(2,1,1);
+%h3=subplot(2,2,1);
 [i1, i4] = seq_match(frq1, frq4);
 plot(frq1(i1), mean(bt4(i4,:) - bt1(i1,:), 2),'LineWidth',1);hold on;
 plot(frq1(i1), mean(bt4_ham(i4,:) - bt1_ham(i1,:),2),'LineWidth',1)
 ax(1)=pv1; ax(2)=pv2; ax(3)=-psf; ax(4)=psf; axis(ax);
 axis([pv1 pv2 -1.5 1.5]); legend('unapodized','Hamming ap.','Location','NorthEast');grid on;
 %xlabel('wavenumber'); 
-ylabel('dBT (K)')
+ylabel('AIRS - CrIS dBT (K)')
 %title(sprintf('AIRS CrIS minus true CrIS %s mean', band));
 grid on; zoom on
 
 % AIRS CrIS minus true CrIS std
-h2=subplot(2,1,2);
+h4=subplot(2,2,4);
 plot(frq1(i1), std(bt4(i4,:) - bt1(i1,:), 0, 2), 'LineWidth',1); hold on;
 plot(frq1(i1), std(bt4_ham(i4,:) - bt1_ham(i1,:),0,2), 'LineWidth',1)
 ax(1)=pv1; ax(2)=pv2; ax(3)=0; ax(4)=psf/2; axis(ax);
 axis([pv1 pv2 0 1.0]); legend('Unapodized','Hamming ap.','Location','North');
-xlabel('wavenumber (cm^{-1})'); ylabel('dBT (K)')
+xlabel('wavenumber (cm^{-1})'); ylabel('AIRS - CrIS dBT (K)')
 %title(sprintf('AIRS CrIS minus true CrIS %s std', band));
 grid on; zoom on
 pname = sprintf('airs_cris_diff_%s_%s', band, app);
@@ -152,3 +154,8 @@ pname = sprintf('airs_cris_diff_%s_%s', band, app);
   pp1=get(h1,'position');set(h1,'position',[pp1(1) pp1(2)-pp1(4)*0.1 pp1(3) pp1(4)*1.1])
   pp2=get(h2,'position');set(h2,'position',[pp2(1) pp2(2)+pp2(4)*0.1 pp2(3) pp2(4)*1.1])
 >
+hold on;
+%%% Then plot the MW band: h3 = subplot(4,2,2) and h4 = subplot(4,2,4)
+  linkaxes([h3 h4],'x');set(h3,'xticklabel','');
+  pp3=get(h3,'position');set(h3,'position',[pp3(1) pp3(2)-pp3(4)*0.1 pp3(3) pp3(4)*1.1])
+  pp4=get(h4,'position');set(h4,'position',[pp4(1) pp4(2)+pp4(4)*0.1 pp4(3) pp4(4)*1.1])
